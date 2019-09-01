ad37dada3c3f9bd64ef685b46e060997a77047fc
jiro4989 <jiroron666@gmail.com>
2019-09-01 22:17:00 +0900

    :tada: init commit

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
+// a
 package main
 
 import (
 	"fmt"
 	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
@@ -11,7 +15,7 @@ type (
 	Target struct {
 		Name       string
 		Path       string
-		Commands   []string
+		Commands   [][]string
 		Extensions []string
 	}
 	Monitrc struct {
@@ -19,10 +23,18 @@ type (
 	}
 )
 
+var (
+	targets map[string]time.Time
+)
+
+func init() {
+	targets = map[string]time.Time{}
+}
+
 func main() {
 	var monitrc Monitrc
 
-	b, err := ioutil.ReadFile(".monitrc")
+	b, err := ioutil.ReadFile(".monit.yml")
 	if err != nil {
 		panic(err)
 	}
@@ -31,5 +43,61 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(monitrc)
+	var i int
+	for {
+		i++
+		fmt.Println(fmt.Sprintf("** %d回目 **", i))
+
+		for _, target := range monitrc.Targets {
+			fmt.Println("[suite] " + target.Name)
+
+			files, err := ioutil.ReadDir(target.Path)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, f := range files {
+				fullPath := filepath.Join(target.Path, f.Name())
+				var defaultTime time.Time
+
+				// 拡張子にマッチするかの判定
+				var found bool
+				for _, ext := range target.Extensions {
+					// Goが返す拡張子には . が先頭に含まれるので除外
+					e := filepath.Ext(fullPath)
+					if len(e) < 1 {
+						break
+					}
+					if e[1:] == ext {
+						found = true
+						break
+					}
+				}
+				if !found {
+					continue
+				}
+
+				t := f.ModTime()
+				// ゼロ値が返却されたら初めてチェックしたとみなす
+				if targets[fullPath] == defaultTime {
+					targets[fullPath] = t
+					continue
+				}
+				// 時間が一致するので変更なし
+				if targets[fullPath] == t {
+					fmt.Println(fmt.Sprintf("%s was not changed.", fullPath))
+					continue
+				}
+
+				fmt.Println(fmt.Sprintf("%s was changed.", fullPath))
+				targets[fullPath] = t
+				for _, cmd := range target.Commands {
+					// エラーが発生しても継続してほしいので無視
+					out, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+					fmt.Println(string(out))
+				}
+			}
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	fmt.Println("ok")
+}

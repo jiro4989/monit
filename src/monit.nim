497ac41cc29633118e8eccc0d5091a92a16359f5
jiro4989 <jiroron666@gmail.com>
2019-09-21 11:57:26 +0900

    何度も実行されるのを修正 #2

--- src/monit.nim
+++ src/monit.nim
@@ -69,6 +69,7 @@ proc run(loopCount = -1, file = ".monit.yml", verbose = false, dryRun = false): 
         for cmd in target.commands:
           debug &"cmd:{cmd}"
           echo execProcess(cmd)
+        break
     sleep conf.sleep * 1000
 
 when isMainModule:

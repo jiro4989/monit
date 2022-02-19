66b9281e736891e8551bb8669f9d518c2c0c9c1a
jiro4989 <jiroron666@gmail.com>
2022-02-19 11:34:27 +0900

    fix test

--- tests/test_monit.nim
+++ tests/test_monit.nim
@@ -1,23 +1,35 @@
-import unittest
+discard """
+  exitCode: 0
+  output: ""
+"""
+
+import std/unittest
 include monit
 
-suite "isExecTargetFile":
-  test "monit.nimは .nim とマッチする":
+block:
+  checkpoint "isExecTargetFile"
+  block:
+    checkpoint "monit.nimは .nim とマッチする"
     check "monit.nim".isExecTargetFile(Target(extensions: @[".nim"]))
     check "monit.nim".isExecTargetFile(Target(extensions: @[".c", ".nim", ".md"]))
-  test "monit.nimはextensions nim とマッチしない":
+  block:
+    checkpoint "monit.nimはextensions nim とマッチしない"
     check not "monit.nim".isExecTargetFile(Target(extensions: @["nim"]))
-  test "monit.nimは monit.nim とする":
+  block:
+    checkpoint "monit.nimは monit.nim とする"
     check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"]))
     check "monit.nim".isExecTargetFile(Target(files: @["main.nim", "monit.nim", "test.nim"]))
-  test "files > exclude_files > extensions > exclude_extensions":
+  block:
+    checkpoint "files > exclude_files > extensions > exclude_extensions"
     check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"],
         exclude_files: @["monit.nim"]))
     check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"],
         exclude_extensions: @["monit.nim"]))
-  test "exclude_files > extensions > exclude_extensions":
+  block:
+    checkpoint "exclude_files > extensions > exclude_extensions"
     check not "monit.nim".isExecTargetFile(Target(exclude_files: @["monit.nim"],
         extensions: @[".nim"]))
-  test "extensions > exclude_extensions":
+  block:
+    checkpoint "extensions > exclude_extensions"
     check "monit.nim".isExecTargetFile(Target(extensions: @[".nim"],
         exclude_extensions: @[".nim"]))

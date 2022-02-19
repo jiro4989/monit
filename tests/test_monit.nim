489e792cf7c96ddf6b1452e73128b8ba16d6ad9a
jiro4989 <jiroron666@gmail.com>
2022-02-19 11:30:25 +0900

    code format

--- tests/test_monit.nim
+++ tests/test_monit.nim
@@ -11,9 +11,13 @@ suite "isExecTargetFile":
     check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"]))
     check "monit.nim".isExecTargetFile(Target(files: @["main.nim", "monit.nim", "test.nim"]))
   test "files > exclude_files > extensions > exclude_extensions":
-    check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"], exclude_files: @["monit.nim"]))
-    check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"], exclude_extensions: @["monit.nim"]))
+    check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"],
+        exclude_files: @["monit.nim"]))
+    check "monit.nim".isExecTargetFile(Target(files: @["monit.nim"],
+        exclude_extensions: @["monit.nim"]))
   test "exclude_files > extensions > exclude_extensions":
-    check not "monit.nim".isExecTargetFile(Target(exclude_files: @["monit.nim"], extensions: @[".nim"]))
+    check not "monit.nim".isExecTargetFile(Target(exclude_files: @["monit.nim"],
+        extensions: @[".nim"]))
   test "extensions > exclude_extensions":
-    check "monit.nim".isExecTargetFile(Target(extensions: @[".nim"], exclude_extensions: @[".nim"]))
+    check "monit.nim".isExecTargetFile(Target(extensions: @[".nim"],
+        exclude_extensions: @[".nim"]))
